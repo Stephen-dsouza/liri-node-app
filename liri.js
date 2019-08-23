8b3b2e605bc5a8ba02daa5ea18209a8f2ef3b1f4
Stephen <stephen.roy.dsouza@gmail.com>
2019-08-23 20:12:00 +1000

    added event search

--- liri.js
+++ liri.js
@@ -3,16 +3,21 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var Spotify = require('node-spotify-api');
-
+var axios = require("axios");
 //get the keys for the API from keys.js
 var spotify = new Spotify(keys.spotify);
-
+var moment = require("moment");
+var fs = require("fs");
+// moment().format();
 var options = process.argv[2];
 var optionsString = process.argv.slice(3).join(" ");
 
 switch (options) {
     case "spotify-this-song":
         spotifyTrack();
+        break;
+    case "concert-this":
+        concerts();
         break;
 }
 
@@ -22,7 +27,7 @@ function spotifyTrack() {
     } else {
         optionsString
     }
-    
+
     spotify.search({
         type: 'track',
         query: optionsString
@@ -31,7 +36,6 @@ function spotifyTrack() {
             return console.log('Error occurred: ' + err);
         }
 
-        // console.log(data.tracks.items[0]);
         var albumName = data.tracks.items[0].album.name;
         var artist = data.tracks.items[0].artists[0].name;
         var preview = data.tracks.items[0].album.preview_url;
@@ -48,9 +52,55 @@ function spotifyTrack() {
             "\nAlbum : " +
             albumName + "\n********************************";
         console.log(output);
-        // console.log(artist);
-        // console.log(preview);
-        // console.log(songName);
     });
+}
 
+
+function concerts() {
+    if (optionsString === "") {
+        console.log("Choose an artist");
+    } else {
+        axios.get("https://rest.bandsintown.com/artists/" + optionsString + "/events?app_id=codingbootcamp").then(
+                function (response) {
+
+                    for (var x = 0; x < response.data.length; x++) {
+
+                        var venue = response.data[x].venue.name;
+                        var venueLocation = response.data[x].venue.city + " , " + response.data[x].venue.country;
+                        var date = moment(response.data[x].datetime).format("DD/MM/YYYY");
+                        var output =
+                            "\n********************************" +
+                            "\nEvent Venue: " +
+                            venue +
+                            "\nLocation of Event: " +
+                            venueLocation +
+                            "\nDate of Event : " +
+                            date +
+                            "\n********************************";
+                        console.log(output);
+                    }
+                    
+                })
+            .catch(function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+
+    }
 }
